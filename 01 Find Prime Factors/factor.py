426f3de05cedfa9eff8dee249cc0102a33999a17
chakrya ros <chakrya.ros@colorado.edu>
2020-05-30 11:04:52 -0600

    add files

---
+++ 01 Find Prime Factors/factor.py
@@ -0,0 +1,16 @@
+def get_prime_factors(N):
+    factors = list()
+    divisor = 2
+    while(divisor <= N):
+        if (N % divisor) == 0:
+            factors.append(divisor)
+            print("N: {}, divisor: {}".format(N, divisor))
+            N = N/divisor
+
+        else:
+            divisor += 1
+    return factors
+    
+if __name__ == '__main__':
+    print(get_prime_factors(630))
+    print(get_prime_factors(13))
